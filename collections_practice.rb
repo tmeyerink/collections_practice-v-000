f8c8e1e5dcea14291cffd3436267a1a66dd63668
Tara Meyerink <tarajmeyerink@gmail.com>
2018-11-03 17:10:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,3 +3,17 @@ def sort_array_asc(arr)
     a <=> b 
   end
 end
+
+def sort_array_asc(arr)
+  arr.sort do |a, b|
+    b <=> a 
+  end
+end
+
+def sort_array_char_count(arr)
+  arr.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(arr)
