367029c6f4f79c4786b364d131d7ec25d88e9008
Tara Meyerink <tarajmeyerink@gmail.com>
2018-11-03 17:23:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,58 +1,41 @@
-def sort_array_asc(arr)
-  arr.sort do |a, b|
-    a <=> b 
+def sort_array_asc(integers)
+  integers.sort
+end
+
+def sort_array_desc(integers)
+  integers.sort {|a,b| b<=>a}
+end
+
+def sort_array_char_count(strings)
+  strings.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(integers)
+  integers.reverse
+end 
+
+def kesha_maker(strings)
+  kesha_strings = []
+  strings.each do |string|
+    string.sub!(string.slice(2), "$")
+    kesha_strings << string
   end
+  kesha_strings
 end
 
-def sort_array_asc(arr)
-  arr.sort do |a, b|
-    b <=> a 
-  end
+def find_a(strings)
+  strings.select {|string| string.start_with?("a")}
 end
 
-def sort_array_char_count(arr)
-  arr.sort do |a, b|
-    a.length <=> b.length
-  end
+def sum_array(integers)
+  integers.inject{|sum, n| sum + n}
 end
 
-def swap_elements(arr)
-  replace = [arr[0], arr[2], arr[1]]
-  return replace
+def add_s(array)
+  array.each_with_index.collect{|element,index| index !=1 ? element +"s" : element}
 end
-
-def reverse_array(arr)
-  return arr.reverse 
-end
-
-def kesha_maker(arr)
-  new_arr = []
-  arr.each do |curr|
-    curr[2] = "$"
-    new_arr << curr
-  end
-  new_arr
-end
-
-def find_a(arr)
-  new_arr = []
-  arr.each do |curr|
-    if curr[0] == "a"
-      new_arr << curr
-    end
-    new_arr
-  end
-end
-
-def sum_array(arr)
-  arr.reduce(:+)
-end
-def add_s(arr)
-  count = 0 
-  arr.each do |curr|
-    #count == 1 ? next: curr.concat("s")
-    curr.concat("s") unless count == 1 
-    cound += 1 
-  end
-end
-
